73320c061406c732b1e04f16699341425490b671
[joshua cochran] <waltwest@outlook.com>
2025-07-20 21:33:13 -0400

    .

---
+++ c.c
@@ -0,0 +1,8 @@
+//standard i/o library
+#include <stdio.h>
+
+//int because it returns 0 to the OS
+int main() {
+    printf("hello world\n");
+    return 0;
+}
---
+++ java.java
@@ -0,0 +1,8 @@
+//class
+public class java{
+    //main method
+    public static void main(String[] args) {
+        //print hello
+        System.out.println("Hello, World!");
+    }
+}
